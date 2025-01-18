6e02af68a3dc818924147bed33616f1cb426740d
rishu9354 <rs682688@gmail.com>
2025-01-18 14:08:35 +0530

    add second program

---
+++ BasicOne.java
@@ -0,0 +1,27 @@
+package Basic;
+// calclute percentage of a given std in cbse borad exam 
+// his marks from 5 subject must be taken as input 
+
+import java.util.Scanner;
+
+class BasicOne{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sub1, sub2, sub3, sub4, sub5;
+        System.out.println("Enter Subject 1 Marks: ");
+        sub1 = sc.nextInt();
+        System.out.println("Enter Subject 2 Marks: ");
+        sub2 = sc.nextInt();
+        System.out.println("Enter Subject 3 Marks: ");
+        sub3 = sc.nextInt();
+        System.out.println("Enter Subject 4 Marks: ");
+        sub4 = sc.nextInt();
+        System.out.println("Enter Subject 5 Marks: ");
+        sub5 = sc.nextInt();
+
+        float percentage  = ((sub1 + sub2 + sub3 + sub4 + sub5) / 500.0f)*100;
+        System.out.println("Percentage : "+ percentage);
+
+
+    }
+}
