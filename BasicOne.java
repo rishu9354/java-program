2903225f5fb4934970ff188899c8830861e0736f
Rishabh Sharma <113090549+rishu9354@users.noreply.github.com>
2025-01-18 15:49:41 +0530

    Update BasicOne.java

--- BasicOne.java
+++ BasicOne.java
@@ -21,7 +21,10 @@ class BasicOne{
         System.out.println("Enter Subject 6 Marks: ");
         int sub6 = sc.nextInt();
 
-        float percentage  = ((sub1 + sub2 + sub3 + sub4 + sub5 + sub6) / 500.0f)*100;
+         System.out.println("Enter Subject 7 Marks: ");
+        int sub7 = sc.nextInt();
+
+        float percentage  = ((sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7) / 700.0f)*100;
         System.out.println("Percentage : "+ percentage);
 
 
