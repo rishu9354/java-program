06167ebab30ae99e8ac167fa5f047f3ab8cfd3f5
rishu9354 <rs682688@gmail.com>
2025-01-23 13:43:20 +0530

    updated

--- Patterns.java
+++ Patterns.java
@@ -105,6 +105,18 @@ public class Patterns {
             System.out.println();
         }
 
-        
+        // 0-1 Triangle
+        System.out.println("0-1 Triangle");
+        for(int i=1;i<=row;i++){
+            for(int j=1; j<=i;j++){
+                int sum =i+j;
+                if (sum % 2 == 0) {
+                    System.out.print("1 ");
+                } else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
