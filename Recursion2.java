b6554052d0ce81a4173edac2320cb0a03c3e71ac
rishu9354 <rs682688@gmail.com>
2025-03-02 17:01:38 +0530

    updated

--- Recursion2.java
+++ Recursion2.java
@@ -1,5 +1,17 @@
 public class Recursion2 {
-    public static void main(String[] args) {
+    public static void towerOfHanoi(int n, String src, String help, String dest){
+        if(n==1){
+            System.out.println("transferring disk "+n+" from "+src+" to "+dest);
+            return;
+        }
         
+        towerOfHanoi(n-1, src, dest, help);
+        System.out.println("transferring disk "+n+" from "+src+" to "+dest);
+        towerOfHanoi(n-1, help, src, dest);
+    }
+    public static void main(String[] args) {
+        System.out.println("Tower Of Hanoi");
+        int n=2;
+        towerOfHanoi(n, "S", "H", "D");
     }
 }
